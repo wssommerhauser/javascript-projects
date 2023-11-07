8bbfffe8c795c720c949febe644326895e65c613
wssommerhauser <130407147+wssommerhauser@users.noreply.github.com>
2023-11-06 20:08:30 -0600

    Add files via upload

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,24 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function(){
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            //console.log(json);
+            for (i = 0; i < json.length; i++){
+            const div = document.getElementById("container");
+            div.innerHTML += `
+        <div class="astronaut">
+            <div class="bio">
+            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+            <ul>
+              <li>Hours in space: ${json[i].hoursInSpace}</li>
+              <li>Active: ${json[i].active}</li>
+              <li>Skills: ${json[i].skills}</li>
+            </ul>
+            </div>
+            <img class="avatar" src="${json[i].picture}">
+        </div>  
+            
+            `;}
+        });
+    });
+});
