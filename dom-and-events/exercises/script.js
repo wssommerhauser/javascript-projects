64477033acbe1b0297d1dda43515091f9f5dadb1
wssommerhauser <130407147+wssommerhauser@users.noreply.github.com>
2023-11-09 19:22:45 -0600

    Add files via upload

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,23 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', liftOff => {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+    missionAbort.addEventListener('mouseover', function (event) {
+        let element = event.target;
+        event.target.style.backgroundColor = "red";
+    });
+    missionAbort.addEventListener('mouseout', function (event) {
+        let element = event.target;
+        event.target.style.backgroundColor = "";
+    });
+    missionAbort.addEventListener('click', abortMission => {
+        //paragraph.innerHTML = "Are you sure you want to abort the mission?";
+        if (confirm("Are you sure you want to abort the mission?") === true){
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";
+        }
+    });
 }
 
 window.addEventListener("load", init);
